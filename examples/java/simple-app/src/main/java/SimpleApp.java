94b0af72a6ac36615c5ecb0d92434e6b9123bc2b
Salim <salim@youos.com>
2018-07-26 11:33:28 +0200

    todos

--- examples/java/simple-app/src/main/java/SimpleApp.java
+++ examples/java/simple-app/src/main/java/SimpleApp.java
@@ -9,6 +9,13 @@ class SimpleApp {
         Config conf = ConfigFactory.parseFile(new File("examples/java/simple-app/src/main/resources/application.conf"));
         Config resolved = conf.resolve();
 
+        // TODO @benjo
+        // ConfigDelayedMerge with kopiert und der Stack verändert
+        // conf = conf.withChangedDefaultValue("app.envtest", ConfigValue)
+        // --> this.root().get("app").withChangedDefaultValue("envtest", ConfigValue)
+        // --> [...] this.root().get("app").replaceChild(...)
+        // conf = conf.withChangedReference("app.envtest", "ENVTEST2") ...
+
         System.out.println(resolved.getValue("app.simple").origin());
         System.out.println(resolved.getValue("app.simple").origin().substitutionPath());
         System.out.println(resolved.getValue("app.simple"));
@@ -16,6 +23,7 @@ class SimpleApp {
         System.out.println();
         System.out.println(resolved.getValue("app.envtest").origin());
         System.out.println(resolved.getValue("app.envtest").origin().substitutionPath() + "  __ SHOULD BE ENVTEST");
+        // TODO substituted value ausgeben
         System.out.println(resolved.getValue("app.envtest"));
 
     }
